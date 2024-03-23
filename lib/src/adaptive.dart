9c8b804d392c2134f7163cf89879a92b8b1cd31b
kingwill101 <hey@glenfordwilliams.com>
2024-03-23 14:59:00 -0500

    introduce AdaptiveContainer Widget

---
+++ lib/src/adaptive.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'media_query.dart';
+
+enum BreakPoint {
+  deafult,
+  xs,
+  sm,
+  md,
+  lg,
+  xl,
+  xxl,
+}
+
+BreakPoint contextToBreakPoint(BuildContext context) {
+  if (context.isExtraSmall) return BreakPoint.xs;
+  if (context.isSmall) return BreakPoint.sm;
+  if (context.isMedium) return BreakPoint.md;
+  if (context.isLarge) return BreakPoint.lg;
+  if (context.isExtraLarge) return BreakPoint.xl;
+  if (context.isExtraExtraLarge) return BreakPoint.xxl;
+
+  return BreakPoint.deafult;
+}
+
+class AdaptiveSlot extends StatelessWidget {
+  final WidgetBuilder builder;
+
+  const AdaptiveSlot({super.key, required this.builder});
+
+  @override
+  Widget build(BuildContext context) {
+    return Builder(
+      builder: builder,
+    );
+  }
+}
+
+class AdaptiveContainer extends StatefulWidget {
+  final Map<BreakPoint, AdaptiveSlot> configs;
+
+  const AdaptiveContainer({Key? key, this.configs = const {}})
+      : super(key: key);
+
+  @override
+  _AdaptiveContainerState createState() => _AdaptiveContainerState();
+}
+
+class _AdaptiveContainerState extends State<AdaptiveContainer> {
+  BreakPoint? _currentBreakpoint;
+  bool hasConfig(BreakPoint breakPoint) =>
+      widget.configs.containsKey(breakPoint);
+  AdaptiveSlot config(BreakPoint breakPoint) => widget.configs[breakPoint]!;
+
+  @override
+  Widget build(BuildContext context) {
+    // Use LayoutBuilder to rebuild the widget when the screen size changes
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final bp = contextToBreakPoint(context);
+
+      // Schedule a callback for the end of this frame to update the state
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          if (_currentBreakpoint == null || bp != _currentBreakpoint) {
+            setState(() {
+              _currentBreakpoint = bp;
+            });
+          }
+        });
+
+        // Use AnimatedSwitcher to animate the transition between widgets
+        return AnimatedSwitcher(
+          duration: const Duration(milliseconds: 300),
+          child: hasConfig(bp) ? config(bp) : SizedBox.shrink(),
+          transitionBuilder: (Widget child, Animation<double> animation) {
+            
+            return FadeTransition(child: child, opacity: animation);
+          },
+        );
+      },
+    );
+  }
+}
